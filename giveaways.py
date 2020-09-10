6407c7e9be2990efa1eff8eed96c71cddded47a2
hinterland-1 <69861817+hinterland-1@users.noreply.github.com>
2020-09-09 21:20:48 -0400

    Add files via upload

--- giveaways.py
+++ giveaways.py
@@ -28,7 +28,7 @@ def scrape_giveaways(session):
 	count = 1
 
 	while True:
-		page = session.get('https://www.goodreads.com/giveaway?sort=ending_soon', params={'page': count})
+		page = session.get('https://www.goodreads.com/giveaway?filter=print&sort=recently_listed&tab=recently_listed', params={'page': count})
 		tree = html.fromstring(page.content)
 		lis = tree.xpath('//li[@class="listElement giveawayListItem"]')
 
@@ -44,29 +44,28 @@ def scrape_giveaways(session):
 				'ID': ID
 			}
 			giveaways.append(giveaway)
+		
 		count = count + 1
 
 	print('[*] {} giveaways scraped.'.format(len(giveaways)))
-	giveaways = [g for g in giveaways if not g['Entered']]
-	print('[*] {} giveaways to enter!'.format(len(giveaways)))
 	return giveaways
 
 
-def enter_giveaway(session, identifier, name=None):
+def enter_giveaway(session, identifier):
 	page = session.get('https://www.goodreads.com/giveaway/enter_choose_address/{}'.format(identifier))
 	tree = html.fromstring(page.content)
 	try:
 		address = int(tree.xpath('//a[@class="gr-button gr-button--small"]/@id')[0][13:])
 	except IndexError:
 		print('''
-[!] Looks like you didn\'t read the README properly. That\'s a shame.
+[!] Looks like you didn\'t read the README properly. That\'s a shame. 
     Here\'s an important and relevant part of the README restated.
 
-Before running the script, make sure you've entered a giveaway manually at least once. The first time involves setting up a new address, and the script assumes that it's done.
+    Before running the script, make sure you've entered a giveaway manually at least once. The first time involves setting up a new address, and the script assumes that it's done.
 			''')
 		sys.exit()
 
-
+	
 	page = session.get('https://www.goodreads.com/giveaway/enter_print_giveaway/{}'.format(identifier), params={'address': address})
 	tree = html.fromstring(page.content)
 	authenticity_token = tree.xpath('//input[@name="authenticity_token"]/@value')[0]
@@ -78,10 +77,9 @@ Before running the script, make sure you've entered a giveaway manually at least
 	}
 	response = session.post('https://www.goodreads.com/giveaway/enter_print_giveaway/{}'.format(identifier),
 							params={'address': address}, data=payload)
-	print('[*] Entered giveaway for: {0} - {1}'.format(identifier, name))
 
 
-def main():
+def main():	
 	session = requests.Session()
 	username = input('[?] Enter your Goodreads username: ')
 	password = getpass('[?] Enter your Goodreads password: ')
@@ -91,10 +89,8 @@ def main():
 
 	for giveaway in giveaways:
 		if not giveaway['Entered']:
-			try:
-				enter_giveaway(session, giveaway['ID'], name=giveaway['Name'])
-			except:
-				print('Couldn\'t do it. :(')
+			enter_giveaway(session, giveaway['ID'])
+			print('[*] Entered giveaway for: {0} - {1}'.format(giveaway['Name'], giveaway['ID']))
 
 
 
